e8772700db5781a56617302a90462f6939fcc282
escribano.cajua <jc060806@gmail.com>
2026-04-24 16:53:14 +0200

    Creamos la carpeta test y dentro la clase FuncionesEvalTest.java

---
+++ src/test/FuncionesEvalTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+class FuncionesEvalTest {
+
+    @Test
+    void contarApariciones() {
+    }
+
+    @Test
+    void encontrarMayor() {
+    }
+}
